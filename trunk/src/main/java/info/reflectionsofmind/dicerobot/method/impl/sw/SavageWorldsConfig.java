2f188925b513be890d3e484d7f341fb2bb5e8a1c
shooshpanchick <shooshpanchick@9dbda60a-bb1c-11de-8784-f7ad4773dbab>
2009-12-04 20:40:01 +0000

    Started Savage Worlds implementation.

---
+++ trunk/src/main/java/info/reflectionsofmind/dicerobot/method/impl/sw/SavageWorldsConfig.java
@@ -0,0 +1,8 @@
+package info.reflectionsofmind.dicerobot.method.impl.sw;
+
+import info.reflectionsofmind.dicerobot.method.IRollConfig;
+
+public class SavageWorldsConfig implements IRollConfig
+{
+
+}
